dcc24521e64d6d9db92e4af38f9f7e5635d051fb
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-09-26 00:52:57 -0300

    Implementação das funcionalidades

--- Projeto/database/__init__.py
+++ Projeto/database/__init__.py
@@ -1,1 +1,80 @@
-# Here will contain the database functions
+import sqlite3
+from sqlite3 import Error
+
+database = 'database/database.db'
+conn = sqlite3.connect(database)
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS categoria (
+  descricao TEXT NOT NULL
+);''')
+conn.commit()
+
+c.execute('''CREATE TABLE IF NOT EXISTS pergunta (
+  id_categoria INTEGER NOT NULL,
+  descricao TEXT NOT NULL,
+  FOREIGN KEY (id_categoria) REFERENCES categoria(rowid) ON UPDATE CASCADE ON DELETE CASCADE
+);''')
+conn.commit()
+
+c.execute('''CREATE TABLE IF NOT EXISTS resposta (
+  id_pergunta INTEGER PRIMARY KEY,
+  descricao TEXT NOT NULL,
+  favorito int DEFAULT 0,
+  FOREIGN KEY (id_pergunta) REFERENCES pergunta(rowid) ON UPDATE CASCADE ON DELETE CASCADE
+);''')
+conn.commit()
+
+def selectCategoryById(categoryId):
+  c.execute("SELECT rowid, descricao FROM categoria WHERE rowid = ?;", str(categoryId))
+  return c.fetchall()
+
+def selectAllCategories():
+  c.execute("SELECT rowid, descricao FROM categoria;")
+  return c.fetchall()
+
+def insertCategory(category):
+  c.execute(f"INSERT INTO categoria VALUES('{category}');")
+  conn.commit()
+
+def deleteCategory(categoryId):
+  c.execute(f"DELETE FROM categoria WHERE rowid = ?;", str(categoryId))
+  conn.commit()
+
+
+def selectQuestionById(questionId):
+  c.execute("SELECT rowid, descricao FROM pergunta WHERE rowid = ?;", str(questionId))
+  return c.fetchall()
+
+def selectAllQuestionsByCategory(categoryId):
+  c.execute("SELECT rowid, descricao FROM pergunta WHERE id_categoria = ?;", str(categoryId))
+  return c.fetchall()
+
+def insertQuestion(categoryId, question):
+  c.execute(f"INSERT INTO pergunta VALUES('{categoryId}', '{question}');")
+  conn.commit()
+
+def deleteQuestion(questionId):
+  c.execute(f"DELETE FROM pergunta WHERE rowid = ?;", str(questionId))
+  conn.commit()
+
+
+def selectAnswerById(answerId):
+  c.execute("SELECT rowid, descricao FROM resposta WHERE rowid = ?;", str(answerId))
+  return c.fetchall()
+
+def selectAllAnswersByQuestionId(questionId):
+  c.execute("SELECT rowid, descricao FROM resposta WHERE id_pergunta = ? ORDER BY favorito DESC;", str(questionId))
+  return c.fetchall()
+
+def insertAnswer(answerId, answers):
+  c.execute(f"INSERT INTO resposta VALUES('{answerId}', '{answers}', 0);")
+  conn.commit()
+
+def incrementAnswerRank(answerId):
+  c.execute(f"UPDATE resposta set favorito = favorito + 1 where rowid = ?;", answerId)
+  conn.commit()
+
+def deleteAnswer(answerId):
+  c.execute(f"DELETE FROM resposta WHERE rowid = ?;", str(answerId))
+  conn.commit()
--- Projeto/main.py
+++ Projeto/main.py
@@ -1,23 +1,233 @@
-import menu_functions as menu
-import interface
-# import database
-interface.limpa_tela()
+from database import *
+
+print('Bem vindo ao PinaTalk!\n')
+
 while True:
-    option_user = menu.create_menu()
-    if option_user == 1:
-        print(option_user)
-        # segue fluxo
-    elif option_user == 2:
-        print(option_user)
-        # segue fluxo
-    # outras opcoes
-    else:
-        interface.limpa_tela()
-        print('Finalizando programa...\n'
-              'Integrantes do projeto: \n'
-              '     Gabriel Gigante\n'
-              '     Gabriel Souza Pereira\n'
-              '     Itanu Romero\n'
-              '     Saulo Martins\n'
-              '     Priscila Barros')
+  print('\nQual tipo de usuário você é?\n')
+  print('1 - Administrador')
+  print('2 - Visitante')
+  print('3 - SAIR')
+
+  userType = str(input('\n'))
+
+  options = ['1', '2', '3']
+
+  if userType == '3':
+    break
+
+  if userType == '1':
+    while True:
+      options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+      print('\nO que você gostaria de fazer?\n')
+      print('1 - Visualizar todas as categorias')
+      print('2 - Inserir nova categoria')
+      print('3 - Remover uma categoria')
+
+      print('\n')
+
+      print('4 - Visualizar perguntas de uma categoria')
+      print('5 - Inserir uma nova pergunta para uma categoria')
+      print('6 - Remover uma pergunta')
+
+      print('\n')
+
+      print('7 - Visualizar respostas de uma pergunta')
+      print('8 - Inserir uma nova resposta para uma pergunta')
+      print('9 - Remover uma resposta')
+
+      print('\n')
+
+      print('10 - SAIR')
+
+      userChoice = str(input('\n'))
+
+      if userChoice not in options:
+        print('\nCódigo inserido não é inválido\n')      
+
+      if userChoice == '1':
+        print(f'\nOpção selecionada: {userChoice} - Visualizar todas as categorias\n')
+
+        categories = selectAllCategories()
+
+        print('ID \tDescrição')      
+        for item in range(len(categories)):
+          print(f'{categories[item][0]}  \t{categories[item][1]}')
+
+      elif userChoice == '2':
+        print(f'\nOpção selecionada: {userChoice} - Inserir nova categoria\n')
+        
+        insertCategory(str(input('Insira o nome da categoria: ')))
+
+        print('\nCategoria inserida\n')
+
+      elif userChoice == '3':
+        print(f'\nOpção selecionada: {userChoice} - Remover uma categoria\n')
+
+        categoryId = str(input('Insira o ID da categoria: '))
+
+        category = selectCategoryById(categoryId)
+
+        if not category:
+          print('\nCategoria não encontrada')
+        else: 
+          deleteCategory(categoryId)
+          print('\nCategoria removida')
+
+      elif userChoice == '4':        
+        print(f'\nOpção selecionada: {userChoice} - Visualizar perguntas de uma categoria\n')
+
+        categoryId = str(input('Insira o ID da categoria: '))
+
+        category = selectCategoryById(categoryId)
+
+        if not category:
+          print('\nCategoria não encontrada')
+        else: 
+          questions = selectAllQuestionsByCategory(categoryId)
+
+          print(f'\nCategoria selecionada: {category[0][1]}')
+          print('\nID \tPergunta')      
+          for item in range(len(questions)):
+            print(f'{questions[item][0]}  \t{questions[item][1]}')
+                        
+      elif userChoice == '5':
+        print(f'\nOpção selecionada: {userChoice} - Inserir uma nova pergunta para uma categoria\n')
+        
+        categoryId = str(input('Insira o ID da categoria: '))
+
+        category = selectCategoryById(categoryId)
+
+        if not category:
+          print('\nCategoria não encontrada')
+        else:
+          print(f'\nCategoria selecionada: {category[0][1]}')
+
+          question = str(input('Informe a pergunta: '))
+          
+          insertQuestion(categoryId, question)
+
+          print('\nPergunta inserida')        
+        
+      elif userChoice == '6':
+        print(f'\nOpção selecionada: {userChoice} - Remover uma pergunta\n')
+
+        questionId = str(input('Insira o ID da pergunta: '))
+
+        question = selectQuestionById(questionId)
+
+        if not question:
+          print('\nPergunta não encontrada')
+        else: 
+          deleteQuestion(questionId)
+          print('\nPergunta removida')
+          
+      elif userChoice == '7':
+        print(f'\nOpção selecionada: {userChoice} - Visualizar respostas de uma pergunta\n')
+
+        questionId = str(input('Insira o ID da pergunta: '))
+
+        question = selectQuestionById(questionId)
+
+        if not question:
+          print('\nPergunta não encontrada')
+        else:
+          print(f'\nPergunta selecionada: {question[0][1]}')
+
+          answers = selectAllAnswersByQuestionId(questionId)
+
+          print('\nID \tResposta')      
+          for item in range(len(answers)):
+            print(f'{answers[item][0]}  \t{answers[item][1]}')
+      
+      elif userChoice == '8':
+        print(f'\nOpção selecionada: {userChoice} - Inserir uma nova resposta para uma pergunta\n')
+
+        questionId = str(input('Insira o ID da pergunta: '))
+
+        question = selectQuestionById(questionId)
+
+        if not question:
+          print('\nPergunta não encontrada')
+        else:
+          print(f'\nPergunta selecionada: {question[0][1]}')
+
+          answer = str(input('Informe a resposta: '))
+          
+          insertAnswer(questionId, answer)
+
+          print('\nResposta inserida')
+      
+      elif userChoice == '9':
+        print(f'\nOpção selecionada: {userChoice} - Inserir uma nova resposta para uma pergunta\n')
+
+        answerId = str(input('Insira o ID da resposta: '))
+
+        answer = selectAnswerById(answerId)
+
+        if not answer:
+          print('\nResposta não encontrada')
+        else: 
+          deleteAnswer(answerId)
+          print('\nResposta removida')
+
+      elif userChoice == '10':
+        break  
+
+  if userType == '2':
+    while True:
+      print('\nQuer tirar uma dúvida? Comece selecionando uma categoria\n')
+    
+      categories = selectAllCategories()
+    
+      print('ID \tDescrição')      
+      for item in range(len(categories)):
+        print(f'{categories[item][0]}  \t{categories[item][1]}')
+    
+      selectedCategoryId = str(input('\nSelecione uma categoria informando seu ID: '))
+    
+      response = selectCategoryById(selectedCategoryId)
+
+      if response:
         break
+    
+    while True:
+      print(f'\nCategoria selecionada: {response[0][1]}')
+
+      questions = selectAllQuestionsByCategory(selectedCategoryId)
+    
+      print('ID \tDescrição')      
+      for item in range(len(questions)):
+        print(f'{questions[item][0]}  \t{questions[item][1]}')
+    
+      selectedQuestionId = str(input('Selecione uma pergunta informando seu ID: '))
+
+      response = selectCategoryById(selectedCategoryId)
+
+      if response:
+        break
+    
+    while True:
+      print(f'\nPergunta selecionada: {response[0][1]}')
+
+      answers = selectAllQuestionsByCategory(selectedCategoryId)
+
+      print('ID \tDescrição')      
+      for item in range(len(answers)):
+        print(f'{answers[item][0]}  \t{answers[item][1]}')
+
+      print('\n0 - Nenhuma das respostas\n')
+    
+      selectedAnswersId = str(input('Eleja uma resposta como a mais útil informando seu ID: '))
+
+      if selectedAnswersId == 0:
+        print('\nEntre em contato com nossos assistentes!')
+        print('00 0000-0000')
+        break
+      else:
+        incrementAnswerRank(selectedAnswersId)
+        print('Obrigado pelo feedback!')
+        break
+
+  if userType not in options:
+    print('\nCódigo inserido não é inválido\n')
